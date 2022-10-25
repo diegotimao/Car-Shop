7421ecaa8ab432d4e3455709e1d92e94468ed7ad
diegotimao <diegotimao1104@gmail.com>
2022-10-25 11:15:21 -0500

    feat: Adicionando rotas e middlwares ao app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandle from './middlewares/error';
+import route from './routes/IcarRoutes';
 
 const app = express();
+
+app.use(express.json());
+app.use(route);
+app.use(errorHandle);
 
 export default app;

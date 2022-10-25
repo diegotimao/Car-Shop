c8f997be9ab50b9a1ca45b08d7285b6e4ab0386a
diegotimao <diegotimao1104@gmail.com>
2022-10-25 16:31:49 -0500

    fix: Corrigindo erro em middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -9,15 +9,15 @@ const errorHandle: ErrorRequestHandler = (
   _next,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({ message: err.issues });
+    return res.status(400).json({ error: err.issues });
   }
 
   const messageAsErrorType = err.message as keyof typeof ErrorTypes;
   const mappedError = errorCatalog[messageAsErrorType];
 
   if (mappedError) {
-    const { httpStatus, message } = mappedError;
-    return res.status(httpStatus).json({ message });
+    const { httpStatus, error } = mappedError;
+    return res.status(httpStatus).json({ error });
   }
 
   return res.status(500).json({ message: 'Error interno' });

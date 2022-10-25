bba8387b3d1cd5b72f574959c61d6a50d93f3745
diegotimao <diegotimao1104@gmail.com>
2022-10-25 11:19:38 -0500

    feat: Icar routes

---
+++ src/routes/IcarRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import ICarController from '../controllers/ICar';
+import IcarService from '../services/ICar';
+import ICarModel from '../models/ICar';
+
+const route = Router();
+
+const icar = new ICarModel();
+const icarService = new IcarService(icar);
+const icarController = new ICarController(icarService);
+
+route.post('/cars', (req, res) => icarController.create(req, res));
+
+export default route;

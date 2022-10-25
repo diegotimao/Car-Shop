d078450aee7cb9c638d0fa56d72b5c0c8ffc148d
diegotimao <diegotimao1104@gmail.com>
2022-10-25 11:16:35 -0500

    feat: Create IcarModel

---
+++ src/models/ICar.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const icarMongooseSchema = new Schema<ICar>({
+  status: Boolean,
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Icar', icarMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Car;

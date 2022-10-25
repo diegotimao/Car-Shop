272157a42aed90960a139415cda4d766d5228328
diegotimao <diegotimao1104@gmail.com>
2022-10-25 15:55:37 -0500

    feat: Controller, metodo que busca um carro por id

--- src/controllers/ICar.ts
+++ src/controllers/ICar.ts
@@ -9,4 +9,9 @@ export default class ICarController {
     const response = await this._service.create(req.body);
     return res.status(201).json(response);
   }
+
+  public async readOne(req: Request, res: Response<ICar | null>) {
+    const response = await this._service.readOne(req.params.id);
+    return res.status(200).json(response);
+  }
 }

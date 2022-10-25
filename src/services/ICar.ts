dbbb741fa0eb203ede0c206551f3911c1424c54e
diegotimao <diegotimao1104@gmail.com>
2022-10-25 11:15:58 -0500

    feat: Create IcarService

---
+++ src/services/ICar.ts
@@ -0,0 +1,29 @@
+import { IService } from '../interfaces/IService';
+import { ICar, ICarSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+
+class IcarService implements IService<ICar> {
+  private _icar:IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._icar = model;
+  }
+
+  public create(obj: unknown): Promise<ICar> {
+    const parsed = ICarSchema.safeParse(obj);
+
+    if (!parsed.success) throw parsed.error;
+
+    return this._icar.create(parsed.data);
+  }
+
+  public async readOne(_id: string): Promise<ICar> {
+    const cars = await this._icar.readOne(_id);
+
+    if (!cars) throw new Error(ErrorTypes.EntityNotFound);
+    return cars;
+  }
+}
+
+export default IcarService;

23d4a11bd4d310242fbb09124a7f99b34000dfc6
diegotimao <diegotimao1104@gmail.com>
2022-10-25 16:30:40 -0500

    fix: Corrigindo catalog de erro

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -4,7 +4,7 @@ export enum ErrorTypes {
 }
 
 type ErrorResponseObject = {
-  message: string;
+  error: string;
   httpStatus: number
 };
 
@@ -14,12 +14,12 @@ export type ErrorCatalog = {
 
 export const errorCatalog: ErrorCatalog = {
   EntityNotFound: {
-    message: 'Entity not found',
+    error: 'Object not found',
     httpStatus: 404,
   },
 
   InvalidMongoId: {
-    message: 'Id notfound',
+    error: 'Id must have 24 hexadecimal characters',
     httpStatus: 400,
   },
 };

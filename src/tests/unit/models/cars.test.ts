0b1c808fb19babea10ee29e79d4623a39fb402eb
diegotimao <diegotimao1104@gmail.com>
2022-10-25 15:57:20 -0500

    fix: Corrigindo erro de importação arquivo de test

--- src/tests/unit/models/cars.test.ts
+++ src/tests/unit/models/cars.test.ts
@@ -1,14 +1,14 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model } from 'mongoose';
-import Car from '../../../models/ICarModel';
+import CarModel from '../../../models/ICarModel';
 import { icarMockReceves, icarMockSubmit } from '../../mocks/IcarsMock';
 
 const { expect } = chai;
 
 
 describe('Camada model Icar', () => {
-  const carModel = new Car();
+  const carModel = new CarModel();
 
   before(async () => {
     sinon.stub(Model, 'create').resolves(icarMockReceves);
@@ -27,6 +27,14 @@ describe('Camada model Icar', () => {
   it('Verifica se ao passar um id por parâmentro ele retorna os dados de um carro', async () => {
     const responseCar = await carModel.readOne('63582c16143d597f738567a7');
     expect(responseCar).to.be.deep.equal(icarMockReceves)
-  })
+  });
+
+  it('Não é possível buscar um carro com id inexistente', async () => {
+    try {
+      await carModel.readOne('1254ee');
+    } catch (error: any) {
+      expect(error.message).to.be.eq('InvalidMongoId')
+    }
+  });
 
 });

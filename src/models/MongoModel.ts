7a6bfbe43e81bc415ca82285aea87f74bdee2a7a
diegotimao <diegotimao1104@gmail.com>
2022-10-26 04:17:33 -0500

    fix: Corrigindo erro que impedia de atualizar

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -25,7 +25,7 @@ abstract class MongoModel<T> implements IModel<T> {
     if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
 
     return this._model.findByIdAndUpdate(
-      { id: _id },
+      { _id },
       { ...obj } as UpdateQuery<T>,
       { new: true },
     );
